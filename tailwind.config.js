ff8494e8c8eccbc0b816069640aa3d2a6bdfbaa4
Ed Madrid <edmadrid@gmail.com>
2024-01-10 13:03:44 -0500

    add theme switching

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "lemonade", "coffee", "retro"],
   },
   plugins: [
     require('daisyui'),
