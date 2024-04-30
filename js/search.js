9795c8184e1dfd145f886538b6661dcae11c14d8
mnyrop <mnyrop@users.noreply.github.com>
2024-04-30 19:01:28 +0000

    deploy: 745c5d5934207fe196f9cb3424fabfc09abb9eef

--- js/search.js
+++ js/search.js
@@ -1,23 +1,29 @@
-var resultsContainer  = document.querySelector ('#results');
-var resultsInfo       = document.querySelector ('#results-info');
-var searchInput       = document.querySelector("#search-input");
+var resultsContainer  = document.getElementById('results');
+var resultsInfo       = document.getElementById('results-info');
+var searchInput       = document.getElementById("search-input");
 var searchLimit       = document.getElementById("search-limit-select");
-var searchSubmit      = document.querySelector("#search-submit");
+var searchSubmit      = document.getElementById("search-submit");
 
 
-function addSearchLimit(query) {
-  console.log(searchLimit);
+function toQueryString(tokens) {
   if (searchLimit.value == 'All Fields') {
-    return query;
+    return tokens.map((token) => `${token}^100 +${token}*^10 ${token}~1`).join(' ')
   }
   else {
-    return `${searchLimit.value.replace(" ", "_")}:${query}`;
+    limitStr = searchLimit.value.replaceAll(" ", "_");
+    return tokens.map((token) => `${limitStr}:${token}^100 +${limitStr}:${token}*^10 ${limitStr}:${token}~1`).join(' ');
   }
 }
 
 function submitSearchQuery(idx) {
-  query = addSearchLimit(searchInput.value || '* *');
-  return idx.search(`${query}~1`) || [];
+  let query = '* *'
+  if (searchInput.value) {
+    input = searchInput.value;
+    tokens = input.split(' ');
+    query = toQueryString(tokens);
+  }  
+  console.log(query);
+  return idx.search(query) || [];
 }
 
 function truncateString(string){
@@ -37,7 +43,7 @@ function appendSearchResults(results, resultsLookupMap) {
     let item = resultsLookupMap[res.ref];
     let resultDiv = document.createElement('div');
     let thumbnail = `https://d1b7k5w7yjwpfg.cloudfront.net/iiif/2/bibliopolitica_${ item.id }_${ item.id }_001/full/250,/0/default.jpg`;
-    resultDiv.innerHTML = `<div class="mb-8"><a href="${prefixUrl}item/${item.id}.html"><img src="${ thumbnail }" alt="thumbnail for ${truncateString(item.label)}" class="max-w-full h-auto"><div class="mt-2 leading-snug md:text-sm text-xs">${truncateString(item.label)}</div></a></div>`;
+    resultDiv.innerHTML = `<div class="mb-8"><a href="${prefixUrl}item/${item.id}.html"><img loading="lazy" src="${ thumbnail }" alt="thumbnail for ${truncateString(item.label)}" class="max-w-full h-auto"><div class="mt-2 leading-snug md:text-sm text-xs">${truncateString(item.label)}</div></a></div>`;
     resultsContainer.appendChild(resultDiv);
   })
 }
@@ -55,6 +61,7 @@ function toDoc(doc) {
       'label': doc.label,
       'Personal_Name': (doc['Personal Name'] || []).join(' '),
       'Corporate_Name': (doc['Corporate Name'] || []).join(' '),
+      'Date_of_Publication_or_Production': (doc['Date of Publication or Production'] || []).join(' '),
       'Narrative_Summary': doc['Narrative Summary'] || '',
       'Subject': (doc['Subject'] || [] ).join(' '),
       'Genre': (doc['Genre'] || [] ).join(' '),
@@ -64,7 +71,6 @@ function toDoc(doc) {
 }
 
 promisedData.then(function(data) {
-  console.log(data);
   const resultsLookupMap = data.reduce(function (memo, doc) {
 	  memo[doc.id] = doc
 	  return memo
@@ -74,20 +80,22 @@ promisedData.then(function(data) {
     this.field('label');
     this.field('Personal_Name');
     this.field('Corporate_Name');
+    this.field('Date_of_Publication_or_Production');
     this.field('Narrative_Summary');
     this.field('Subject');
     this.field('Genre');
     this.field('Format');
     this.field('Keyword');
 
+    this.pipeline.remove(lunr.stemmer);
+    this.searchPipeline.remove(lunr.stemmer);
+
     data.forEach(function (doc) {
       this.add(toDoc(doc))
     }, this)
   })
-  console.log(idx.toJSON());
   // default: start with all results shown
   results = submitSearchQuery(idx);
-  console.log(results);
   appendSearchResults(results, resultsLookupMap);
 
   document.body.addEventListener('keypress', function(e) {
@@ -98,6 +106,16 @@ promisedData.then(function(data) {
     }
   });
   searchSubmit.addEventListener('click', function() { 
+    results = submitSearchQuery(idx);
+    appendSearchInfo(results);
+    appendSearchResults(results, resultsLookupMap);
+  }, false);
+  searchInput.addEventListener('keyup', function() { 
+    results = submitSearchQuery(idx);
+    appendSearchInfo(results);
+    appendSearchResults(results, resultsLookupMap);
+  }, false);
+  searchLimit.addEventListener('change', function() {
     results = submitSearchQuery(idx);
     appendSearchInfo(results);
     appendSearchResults(results, resultsLookupMap);
