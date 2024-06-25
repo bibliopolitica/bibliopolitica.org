2431920b50d284daf725593b8fbf799db0f49c86
mnyrop <marii@nyu.edu>
2024-06-25 18:15:50 -0400

    run axe on more pages

--- axe.sh
+++ axe.sh
@@ -1,7 +1,7 @@
 npm exec axe https://bibliopolitica.github.io/ 
-# npm exec axe https://bibliopolitica.github.io/about/project.html 
-# npm exec axe https://bibliopolitica.github.io/about/library-timeline.html 
-# npm exec axe https://bibliopolitica.github.io/browse/items.html 
-# npm exec axe https://bibliopolitica.github.io/exhibit/en/1.html 
-# npm exec axe https://bibliopolitica.github.io/item/CSL00094.html 
-# npm exec axe https://bibliopolitica.github.io/history/OH001.html
+npm exec axe https://bibliopolitica.github.io/about/project.html 
+npm exec axe https://bibliopolitica.github.io/about/library-timeline.html 
+npm exec axe https://bibliopolitica.github.io/browse/items.html 
+npm exec axe https://bibliopolitica.github.io/exhibit/en/1.html 
+npm exec axe https://bibliopolitica.github.io/item/CSL00094.html 
+npm exec axe https://bibliopolitica.github.io/history/OH001.html
